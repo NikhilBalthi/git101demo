b605a0af8533b5bda6b8fe1c43bce9042ed140b6
NikhilBalthi <86175945+NikhilBalthi@users.noreply.github.com>
2024-11-29 17:56:52 -0800

    Delete FirstOne.java

--- FirstOne.java
+++
@@ -1,9 +0,0 @@
-public class FirstOne {
-    public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("just trying");
-        System.out.println("hello third");
-        System.out.println("hello four");
-        System.out.println("hello five");
-    }
-}
